cb053bb4165828b82cb0894ca55adb2f6ecaca99
andrazpolak <andrazpolak@gmail.com>
2021-04-27 20:12:41 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,80 @@
+//ATP util
+function isArrayNotEmpty(array) {
+    return Array.isArray(array) && array.length > 0;
+};
+
+function Convert16bitesArray(array, output_type) {
+    if (!isArrayNotEmpty(array))
+        return;
+
+    if (!output_type)
+        return array[0];
+
+    let ret = array[0];
+
+    switch (output_type) {
+        case "uint":
+        case "uint16":
+            if (array.length === 1)
+                ret = (array[0] & 0xFFFF);
+            break;
+        case "int":
+        case "int16":
+            if (array.length === 1) {
+                ret = (array[0] & 0xFFFF);
+                if (array[0] & (1 << 15))
+                    ret |= 0xFFFF0000;
+            }
+            break;
+        case "int32":
+            if (array.length === 2) {
+                ret = (((array[1] & 0xFFFF) << 16) | (array[0] & 0xFFFF));
+                if (array[1] & (1 << 15))
+                    ret |= 0xFFFFFFFF00000000;
+            }
+            break;
+        case "uint32":
+            if (array.length === 2) {
+                ret = (((array[1] & 0xFFFF) << 16) | (array[0] & 0xFFFF));
+            }
+            break;
+        case "string":
+        case "string8bit":
+            if (array.length > 0) {
+                const tmp_stringArray = [];
+                for (let item of array) {
+                    const char1 = item & 0x00FF;
+                    const char2 = (item & 0xFF00) >> 8;
+
+                    if (char1 === 0) break;
+                    tmp_stringArray.push(String.fromCharCode(char1))
+                    if (char2 === 0) break;
+                    tmp_stringArray.push(String.fromCharCode(char2))
+
+                }
+                ret = tmp_stringArray.join("");
+            }
+            break;
+        case "string16bit":
+            if (array.length > 0) {
+                const tmp_stringArray = [];
+                for (let item of array) {
+                    if (item === 0) break;
+                    tmp_stringArray.push(String.fromCharCode(item))
+                }
+                ret = tmp_stringArray.join("");
+            }
+            break;
+        //TODO: add support for float
+        default:
+    }
+
+    return ret;
+};
+
+
+module.exports = {
+    isArrayNotEmpty,
+    Convert16bitesArray
+}
+
